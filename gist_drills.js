105ceb07494dc225da4f8feff9789eb17d0c3b8f
BuddhaL1T3 <jmriggs75@gmail.com>
2018-05-09 15:27:33 -0400

    added object drill #2

--- gist_drills.js
+++ gist_drills.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//1. object initializers and methods
+
 const loaf = {
   flour: 300,
   water: 210,
@@ -11,3 +13,24 @@ const loaf = {
 console.log(loaf.flour);
 console.log(loaf.water);
 console.log(loaf.hydration());
+
+////////////////////////////////////////////////////
+
+//2. Iterating over an object's properties
+
+const object = {
+  foo: 1,
+  bar: 2,
+  fum: 3,
+  quux: 4,
+  spam: 5,
+}
+for (let key in object) {
+    const value = object[key];
+    console.log(`${key}: ${value}`);
+}
+
+/////////////////////////////////////////////////
+
+//3. Arrays in objects
+
