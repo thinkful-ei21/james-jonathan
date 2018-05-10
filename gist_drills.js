12265405ac79af6beb88b80d0c9e2742afb52847
BuddhaL1T3 <jmriggs75@gmail.com>
2018-05-09 21:31:58 -0400

    modifications to existing functions and started function 7

--- gist_drills.js
+++ gist_drills.js
@@ -67,24 +67,32 @@ const myArr = [
   }
 ];
 
-for (let i = 0; i < myArr.length; i++) {
-  console.log(`This is my name: ${myArr[i].name} and this my job: ${myArr[i].jobTitle}!`);
-}
+myArr.forEach(function(item) {
+  console.log(`This is my name: ${item.name} and this my job: ${item.jobTitle}!`);
+})
+
+//for (let i = 0; i < myArr.length; i++) {
+ // console.log(`This is my name: ${myArr[i].name} and this my job: ${myArr[i].jobTitle}!`);
+//}
 
 /////////////////////////////////////////////////
+
 // 5. Properties that aren't there
 
 for (let i = 0; i < myArr.length; i++) {
     const person = myArr[i];
-    if (person.jobTitle === 'Owner') {
-        person.boss = 'nobody';
-    }
     if (person.jobTitle !== 'Owner') {
-        person.boss = myArr[i-1].name;
+      person.boss = myArr[i-1].name;
     }
-
+    if (person.jobTitle === 'Owner') {
+      //person.boss = 'nobody';
+      console.log(`${person.jobTitle} ${person.name} reports to nobody.`)
+    }
+    else{
     console.log(`${person.jobTitle} ${person.name} reports to ${person.boss}.`);
+    }  
 }
+
 
 //////////////////////////////////////////////////////
 
@@ -121,3 +129,26 @@ console.log (decode('craft block argon meter bells brown croon droop'));
 /////////////////////////////////////////////
 
 //7. Factory Functions with LOTR
+
+const createCharacters = (name, nickname, race, origin, attack, defence) => {
+  return {
+    name,
+    nickname,
+    race,
+    origin,
+    attack,
+    defence,
+    describe: function () {
+      console.log(`${this.name} is a ${this.race} from ${this.origin}.`);
+    }, 
+    evaluateFight: function (character) {
+      console.log(`"Your opponent takes ${(this.attack - character.defence < 0) ? 0: this.attack - character.defence} damage 
+and you receive ${(character.attack - this.defence < 0) ? 0: character.attack - this.defence} damage"`);
+    },
+  } 
+}
+const gandalf = createCharacters('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6);
+const bilbo = createCharacters('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1);
+
+//console.log(gandalf.describe());
+console.log(bilbo.evaluateFight(gandalf));
